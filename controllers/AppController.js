0ccb3a27040f089f89246c3b272c78ea3fead2cf
davykoch <koechdavis24@gmail.com>
2024-08-07 01:30:11 +0300

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,7 +5,7 @@ class AppController {
   static getStatus(req, res) {
     const status = {
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive()
+      db: dbClient.isAlive(),
     };
     res.status(200).json(status);
   }
@@ -17,10 +17,10 @@ class AppController {
       }
       const usersCount = await dbClient.nbUsers();
       const filesCount = await dbClient.nbFiles();
-      res.status(200).json({ users: usersCount, files: filesCount });
+      return res.status(200).json({ users: usersCount, files: filesCount });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
 }
