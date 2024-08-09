821344abfdbd5758eb22c57df5e980e59cdec1ec
davykoch <koechdavis24@gmail.com>
2024-08-10 00:45:58 +0300

    Create a new user

--- utils/db.js
+++ utils/db.js
@@ -5,24 +5,34 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
+    const url = `mongodb://${host}:${port}/${database}`;
 
     this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.client.connect((err) => {
-      if (err) console.log(err);
-      this.db = this.client.db(database);
-    });
+    this.db = null;
+    this.connect();
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      this.db = this.client.db();
+      console.log('Connected to MongoDB');
+    } catch (err) {
+      console.error('Failed to connect to MongoDB:', err);
+    }
   }
 
   isAlive() {
-    return !!this.client && !!this.client.topology && this.client.topology.isConnected();
+    return !!this.client && !!this.db;
   }
 
   async nbUsers() {
+    if (!this.db) await this.connect();
     return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
+    if (!this.db) await this.connect();
     return this.db.collection('files').countDocuments();
   }
 }
