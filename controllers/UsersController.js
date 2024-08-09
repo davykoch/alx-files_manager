63a2032f018484ba1f212f510daa4e439f57e176
davykoch <koechdavis24@gmail.com>
2024-08-10 00:36:52 +0300

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -13,7 +13,8 @@ class UsersController {
     if (!email) return res.status(400).json({ error: 'Missing email' });
     if (!password) return res.status(400).json({ error: 'Missing password' });
 
-    const userExists = await dbClient.db.collection('users').findOne({ email });
+    const usersCollection = dbClient.db.collection('users');
+    const userExists = await usersCollection.findOne({ email });
     if (userExists) return res.status(400).json({ error: 'Already exist' });
 
     const hashedPassword = sha1(password);
